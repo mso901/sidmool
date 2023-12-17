778de07539599858ce92f2c8c86b1a9374ad44cd
seonokmin <dnsmsdkdl@gmail.com>
2023-12-17 21:26:59 +0900

    add pomotion section

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,29 @@
-$(function(){
-  var swiper = new Swiper(".mySwiper", {
+/** @format */
+
+$(function () {
+  //main_visual
+  var swiper = new Swiper('.main_visual', {
     spaceBetween: 30,
     loop: true,
-    effect: "fade",
+    effect: 'fade',
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
+    },
+  });
+  //promotion
+  var swiper = new Swiper('.event_slide', {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
   });
 });

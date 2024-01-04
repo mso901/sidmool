a0fff02ee8de8acef362dd607ee4bbce721bb4d7
minsunok <dnsmsdkdl@gmail.com>
2024-01-04 14:29:53 +0900

    mo_done

--- js/slide.js
+++ js/slide.js
@@ -2,16 +2,44 @@
 
 $(function () {
   //main_visual
-  var swiper = new Swiper('.main_visual', {
-    spaceBetween: 30,
-    loop: true,
-    effect: 'fade',
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-  });
+  // var swiper = new Swiper('.main_visual', {
+  //   spaceBetween: 30,
+  //   loop: true,
+  //   effect: 'fade',
+  //   autoplay: {
+  //     delay: 2000,
+  //     disableOnInteraction: false,
+  //   },
+  // });
+  let i = 0;
+  function slide(){
+    if(i >= 3){
+      i = 0;
+    } else {
+      i++;
+    };
+    $(".main_visual .swiper-slide").fadeOut(4000);
+    $(".main_visual .swiper-slide").eq(i).fadeIn(4000);
+  };
+  setInterval(slide, 10000)
+
   //promotion
+   let Width = 800;
+  let screenWidth = $(window).width();
+  if (screenWidth < Width) {
+    var swiper = new Swiper(".event_slide", {
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+    });
+    var swiper = new Swiper(".sns_img", {
+      effect: "cards",
+      grabCursor: true,
+      loop: true,
+    });
+  } else {
   var swiper = new Swiper('.event_slide', {
     slidesPerView: 2,
     spaceBetween: 0,
@@ -26,4 +54,5 @@ $(function () {
       clickable: true,
     },
   });
+};//else
 });

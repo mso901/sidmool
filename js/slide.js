2f9c99b6948893b990b7d7f50d9dc20f0617075d
minsunok <dnsmsdkdl@gmail.com>
2024-01-05 10:28:00 +0900

    call gone

--- js/slide.js
+++ js/slide.js
@@ -12,47 +12,49 @@ $(function () {
   //   },
   // });
   let i = 0;
-  function slide(){
-    if(i >= 3){
+  function slide() {
+    if (i >= 3) {
       i = 0;
     } else {
       i++;
-    };
+    }
     $(".main_visual .swiper-slide").fadeOut(4000);
     $(".main_visual .swiper-slide").eq(i).fadeIn(4000);
-  };
-  setInterval(slide, 10000)
+  }
+  setInterval(slide, 10000);
 
   //promotion
-   let Width = 800;
-  let screenWidth = $(window).width();
-  if (screenWidth < Width) {
-    var swiper = new Swiper(".event_slide", {
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-      },
-    });
-    var swiper = new Swiper(".sns_img", {
-      effect: "cards",
-      grabCursor: true,
-      loop: true,
-    });
-  } else {
-  var swiper = new Swiper('.event_slide', {
-    slidesPerView: 2,
-    spaceBetween: 0,
-    centeredSlides: true,
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
-};//else
+  let Width = 800;
+  $(window).on("load resize", function () {
+    let screenWidth = $(window).width();
+    if (screenWidth < Width) {
+      var swiper = new Swiper(".event_slide", {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+        },
+      });
+      var swiper = new Swiper(".sns_img", {
+        effect: "cards",
+        grabCursor: true,
+        loop: true,
+      });
+    } else {
+      var swiper = new Swiper(".event_slide", {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    } //else
+  }); //slide
 });
